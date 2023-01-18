2f4c9d406120c819148847eed6ca8ec08a7a5b03
andrei.kandratsenka <anddrews@tut.by>
2023-01-18 20:29:49 +0200

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,49 @@
-import { numMapEn, numMapRu } from "./modules/dictionary.js";
-const num = 4677767;
+const btnId = document.getElementById('button');
+const btnTag = document.getElementsByTagName('button');
+const btnClass = document.getElementsByClassName('button test');
 
-function numToString(num, numMap) { 
-  return num
-  .toString()
-  .split('')
-  .reduce((acc, el, index) => index === 0 ? numMap[el] : `${acc} ${numMap[el]}`, '');
+// console.log(btnId);
+// console.log(btnTag);
+// console.log(btnClass);
+
+const btnQuerySelector = document.querySelector('.button.test');
+const span = btnQuerySelector.querySelector('span');
+const btnQuerySelectorAll = document.querySelectorAll('.button');
+
+// console.log(btnQuerySelector);
+// console.log(btnQuerySelectorAll);
+
+
+// btnQuerySelector.style.backgroundColor = '#ad2345';
+btnQuerySelector.classList.toggle('smth');
+
+const newChildren = document.createElement('span');
+const newEl = document.createElement('span');
+
+newEl.textContent = 'alert("hello world")';
+newEl.appendChild(newChildren);
+newEl.classList.add('boo');
+newEl.id = 'fghfkhfk';
+
+const body = document.querySelector('body');
+btnQuerySelector.insertBefore(newEl, span);
+
+const textarea = document.querySelector('.textarea');
+
+const nodeArr = [];
+
+const fragment = document.createDocumentFragment();
+
+for(let i = 0; i < 5; i++) {
+  let el = document.createElement('span');
+  el.innerText = 'test';
+  const r = fragment.appendChild(el);
 }
 
-console.log(numToString(num, numMapRu));
+textarea.append('fragment');
+
+
+
+
+
+
