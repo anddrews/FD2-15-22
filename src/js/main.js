3a8fd075224e040da93d2ae3dab27b78c3861c8f
andrei.kandratsenka <anddrews@tut.by>
2023-02-04 09:59:04 +0200

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,8 @@
-import { cardsMock } from "./modules/cardsMock.js";
-import { Renderer } from "./modules/renderer.js";
+import Calculator from "./modules/view.js";
 
-const root = document.querySelector('#root');
+const buttons = [7, 8, 9, '+', 4, 5, 6, '-', 1, 2, 3, '*', 0 , ',', '=', '/'];
+const root = document.querySelector('.root');
+const calculator = new Calculator(buttons, root);
 
-const renderer = new Renderer(root);
-
-// renderer.render();
-// renderer.renderCards([cardsMock[0]]);
-// renderer.renderCards(cardsMock);
-
-const canvas = document.querySelector('.canvas');
-
-const ctx = canvas.getContext('2d');
-
-ctx.fillStyle = "rgb(200,0,0)";
-
-ctx.fillRect (10, 10, 55, 50);
-
-ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
-ctx.strokeRect (30, 30, 55, 50);
-ctx.fillStyle = "rgb(0, 0, 0)";
-ctx.fillText("Hello world", 10, 50);
+calculator.render();
+calculator.setDisplay('1000');
