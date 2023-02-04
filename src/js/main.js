0042ccae64b602320f15567aa507ce8a07bbbcf0
andrei.kandratsenka <anddrews@tut.by>
2023-02-04 08:52:36 +0200

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -5,9 +5,19 @@ const root = document.querySelector('#root');
 
 const renderer = new Renderer(root);
 
-renderer.render();
+// renderer.render();
+// renderer.renderCards([cardsMock[0]]);
+// renderer.renderCards(cardsMock);
 
+const canvas = document.querySelector('.canvas');
 
-renderer.renderCards([cardsMock[0]]);
-renderer.renderCards(cardsMock);
+const ctx = canvas.getContext('2d');
 
+ctx.fillStyle = "rgb(200,0,0)";
+
+ctx.fillRect (10, 10, 55, 50);
+
+ctx.fillStyle = "rgba(0, 0, 200, 0.5)";
+ctx.strokeRect (30, 30, 55, 50);
+ctx.fillStyle = "rgb(0, 0, 0)";
+ctx.fillText("Hello world", 10, 50);
