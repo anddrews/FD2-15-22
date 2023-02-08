aa9806a9f5733498f284664f9ba982315bd8ba9b
andrei.kandratsenka <anddrews@tut.by>
2023-02-08 18:37:10 +0200

    instanceof and type of

--- src/js/main.js
+++ src/js/main.js
@@ -9,12 +9,22 @@ const view = new View(buttons, root);
 const calculator = new Calculator();
 const controller = new Controller(calculator, view);
 
-controller.initialize();
+// controller.initialize();
 
 
 const obj = {
-    name: 'Alex',
+    _name: 'Alex',
+    get name() {
+        return this._name;
+    },
+    set name(name) {
+        this._name = name + ' smth';
+    }
 }
+
+obj.name = "Jhon";
+
+// console.log(obj);
 
 // obj.getName();
 
@@ -35,18 +45,88 @@ class UserClass {
     setName(name) {
         this.#name = name;
     }
+
+    // get name() {
+    //     return this.#name.toUpperCase();
+    // }
+
+    set name(name) {
+        if (typeof name !== 'string') {
+            throw new Error('Name should be a string value');
+        }
+
+        this.#name = name;
+    }
+
+    greeting() {
+        console.log(`Hi!, I'm ${this.#name}`);
+    }
 }
 
-const user = new User('John');
-const user2 = new UserClass('Alex');
-user.name = 'Bob';
-user2.setName('Bob');
-console.log(user);
-console.log(user2);
-user.getName();
+class Student extends UserClass {
+    #course;
+    constructor(name, course) {
+        super(name);
+        this.#course = course;
+    }
+
+    get name() {
+        return `Student name ${super.name}`;
+    }
+
+    set name(name) {
+        super.name = name;
+    }
+
+}
+
+// class Teacher {
+//     #user;
+//     #topic;
+
+//     constructor(name, topic) {
+//         this.#user = new UserClass(name);
+//         this.#topic = topic;
+//     }
+
+//     get name() {
+//         return this.#user.name;
+//     }
+
+//     set name(name) {
+//         this.#user.name = name;
+//     }
+    
+// }
+
+class Teacher extends UserClass {
+    #topic;
+    constructor(name, topic) {
+        super(name);
+        this.#topic = topic;
+    }
+
+    get name() {
+        return super.name;
+    }
+
+    set name(name) {
+        super.name = name;
+    }
+}
 
 
+const student = new Student('Alex', 5);
+const user = new UserClass("Alex");
 
+console.log(student);
+console.log(student.name);
+
+student.name = '121231';
+student.setName('ererer');
+student.greeting();
+
+console.log(student instanceof UserClass)
 
 
 
