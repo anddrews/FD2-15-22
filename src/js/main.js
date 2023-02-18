9c123f051e15f4ef18e8e68ffc4c15d051f1d80a
andrei.kandratsenka <anddrews@tut.by>
2023-02-18 10:23:24 +0200

    initial commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,41 @@
-import {request} from './modules/request.js';
-import {getCard} from './modules/card.js';
+function all(arr) {
+    // return arr
+    //     .reduce(
+    //         (acc, item) => acc
+    //         .then(data => item.then(v => [...data, v])),
+    //         Promise.resolve([])
+    //     )
 
-let items = [];
-const container = document.querySelector('.container');
 
-request('objects')
-.then(data => {
-    items = data;
-    items.forEach(({name, data}) => {
-        let price;
+    return new Promise((res, rej) => {
+        const results = [];
+        let count = 0;
 
-        if (data && (data.Price || data.price)) {
-            price = data.Price || data.price;
-        }
-        const card = getCard(name, price);
-        container.appendChild(card);
+        for (let i = 0; i < arr.length; i++) {
+            arr[i]
+                .then(data => {
+                    results[i] = data;
+                    count++;
+                    if(arr.length === count) {
+                        res(results);
+                    }
+                    return data;
+                })
+                .catch(e => rej(e));
+        }        
     });
-})
+}
+
+// Promise.resolve(1);
+// Promise.reject(1);
+const prom1 = new Promise((res, rej) => { 
+    for(let i = 0; i === Infinity; i++){}
+    res(1)
+    // setTimeout(() => res(1), 1500);
+    })
+const prom2 = new Promise((res, rej) => {
+     setTimeout(() => res(2), 1000);
+    })
+
+all([prom1, prom2, prom2]).then(res => console.log(res))
+    
