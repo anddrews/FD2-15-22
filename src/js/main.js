a741e32d4f6af49115fa0c210f84263bc1db64df
andrei.kandratsenka <anddrews@tut.by>
2023-02-18 11:24:06 +0200

    smth

--- src/js/main.js
+++ src/js/main.js
@@ -16,26 +16,75 @@ function all(arr) {
                 .then(data => {
                     results[i] = data;
                     count++;
-                    if(arr.length === count) {
+                    if (arr.length === count) {
                         res(results);
                     }
                     return data;
                 })
                 .catch(e => rej(e));
-        }        
+        }
     });
+}
+
+
+async function allAwait(arr) {
+    const results = [];
+    for await(let prom of arr) {
+        results.push(prom)
+    }
+    
+    return results;
 }
 
 // Promise.resolve(1);
 // Promise.reject(1);
-const prom1 = new Promise((res, rej) => { 
-    for(let i = 0; i === Infinity; i++){}
+const prom1 = new Promise((res, rej) => {
+    for (let i = 0; i === Infinity; i++) { }
     res(1)
     // setTimeout(() => res(1), 1500);
-    })
+})
 const prom2 = new Promise((res, rej) => {
-     setTimeout(() => res(2), 1000);
-    })
+    setTimeout(() => res(2), 1000);
+})
 
-all([prom1, prom2, prom2]).then(res => console.log(res))
-    
+async function t() {
+    const res = await allAwait([prom1, prom2])
+    console.log('sss', res);
+}
+
+t();
+
+// all([prom1, prom2, prom2]).then(res => console.log(res))
+
+function sum(prom1, prom2) {
+    return new Promise((res, rej) => {
+        res()
+    })
+}
+
+prom2
+    .then(res => {
+        console.log(res)
+    })
+    .catch(e => console.log(e));
+
+async function fn() {
+    try {
+        const res = await prom2;
+        console.log(res);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+fn();
+
+
+const onResolve = param => console.log(param);
+const onReject = param => console.log(param);
+
+const newProm = new Promise((res, rej) => {
+    res('resolved'); //onResolve('resolved')
+})
+
+newProm.then(onResolve);
