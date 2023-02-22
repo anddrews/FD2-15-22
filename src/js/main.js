0f2278bbd3b4be6f48d756331e0139fed39ee48a
andrei.kandratsenka <anddrews@tut.by>
2023-02-22 20:35:27 +0200

    delete cards

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,76 @@
-import {request} from './modules/request.js';
-import {getCard} from './modules/card.js';
+import { request } from './modules/request.js';
+import { getCard } from './modules/card.js';
 
 let items = [];
-const container = document.querySelector('.container');
+const container = document.querySelector('.container__items');
 
-request('objects')
-.then(data => {
-    items = data;
-    items.forEach(({name, data}) => {
+container.addEventListener('click', ({target}) => {
+    const {id: action, dataset: { id }} = target;
+
+    if (action === 'delete') {
+        target.parentElement.parentElement.remove();
+        const index = items.findIndex(({id}) => id === target.id);
+        items.splice(index - 1, 1);
+        // items = items.filter(item => item.id !== id);
+    }
+
+
+
+    console.log(action, id)
+})
+
+const renderItems = (items) => {
+    const elements = items.map(({ name, data, id }) => {
         let price;
 
         if (data && (data.Price || data.price)) {
             price = data.Price || data.price;
         }
-        const card = getCard(name, price);
-        container.appendChild(card);
+        return getCard(name, price, id);
     });
+    container.replaceChildren(...elements);
+}
+
+request('objects')
+    .then(data => {
+        items = data;
+        renderItems(items);
+    })
+
+const form = document.querySelector('#addItem');
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    const name = e.target.elements.name.value;
+    const price = e.target.elements.price.value;
+
+    request(
+        'objects',
+        'POST',
+        {
+            name,
+            data: {
+                price
+            }
+        }).then(item => {
+            e.target.elements.name.value = '';
+            e.target.elements.price.value = '';
+            items.push(item);
+            renderItems(items);
+        })
+
+}
+
+form.addEventListener('submit', onSubmit);
+
+
+const editBtns = container.querySelectorAll('#edit');
+
+console.log('buttons', editBtns);
+
+editBtns.forEach(btn => {
+    btn.addEventListener('click', () => console.log('edit', btn.dataset.id))
 })
+
+
--- src/js/modules/card.js
+++ src/js/modules/card.js
@@ -1,4 +1,4 @@
-function getCard(name, price) {
+function getCard(name, price, id) {
     const card = document.createElement('div');
     card.classList.add('container__row');
     card.innerHTML = `<div class="container__column">
@@ -8,7 +8,8 @@ function getCard(name, price) {
             ${price || 'Empty'}
         </div>
         <div class="container__column">
-            Actions
+            <button id="edit" data-id="${id}">Edit</button>
+            <button id="delete" data-id="${id}">Delete</button>
         </div>`;
 
   return card;
