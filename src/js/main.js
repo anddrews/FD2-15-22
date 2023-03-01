a46c904ed10840ed5c7ba1fb56d370067a92578d
andrei.kandratsenka <anddrews@tut.by>
2023-03-01 19:10:21 +0200

    intersection observer

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,8 @@
-function debounce(fn, delay) {
-    let allowed = true;
-
-    return function(...arg) {
-        if (allowed) {
-            fn(...arg);
-            allowed = false;            
-            setTimeout(() => {
-                allowed = true;
-            }, delay);
-        }
+var observer = new IntersectionObserver((entries) => {
+    if (entries[0].intersectionRatio >= 0) {
+        document.querySelector('.header')
+        .classList.toggle('active')
     }
-}
+})
 
-function logger(a, b) {
-    console.log(`logger: ${a} ${b}`);
-}
-
-const debounced = debounce(logger, 100);
-
-debounced(1, 2);
-debounced(3, 4);
-debounced(3, 4);
-debounced(3, 4);
-debounced(3, 4);
-debounced(3, 4);
-
-setTimeout(() => debounced(5, 6), 100)
+observer.observe(document.querySelector('.item'));
