89640c74a3bac04150eadf3924aa21f368f5debf
andrei.kandratsenka <anddrews@tut.by>
2023-03-01 18:35:26 +0200

    debounce

--- src/js/main.js
+++ src/js/main.js
@@ -1,76 +1,28 @@
-import { request } from './modules/request.js';
-import { getCard } from './modules/card.js';
+function debounce(fn, delay) {
+    let allowed = true;
 
-let items = [];
-const container = document.querySelector('.container__items');
-
-container.addEventListener('click', ({target}) => {
-    const {id: action, dataset: { id }} = target;
-
-    if (action === 'delete') {
-        target.parentElement.parentElement.remove();
-        const index = items.findIndex(({id}) => id === target.id);
-        items.splice(index - 1, 1);
-        // items = items.filter(item => item.id !== id);
-    }
-
-
-
-    console.log(action, id)
-})
-
-const renderItems = (items) => {
-    const elements = items.map(({ name, data, id }) => {
-        let price;
-
-        if (data && (data.Price || data.price)) {
-            price = data.Price || data.price;
+    return function(...arg) {
+        if (allowed) {
+            fn(...arg);
+            allowed = false;            
+            setTimeout(() => {
+                allowed = true;
+            }, delay);
         }
-        return getCard(name, price, id);
-    });
-    container.replaceChildren(...elements);
+    }
 }
 
-request('objects')
-    .then(data => {
-        items = data;
-        renderItems(items);
-    })
-
-const form = document.querySelector('#addItem');
-
-function onSubmit(e) {
-    e.preventDefault();
-
-    const name = e.target.elements.name.value;
-    const price = e.target.elements.price.value;
-
-    request(
-        'objects',
-        'POST',
-        {
-            name,
-            data: {
-                price
-            }
-        }).then(item => {
-            e.target.elements.name.value = '';
-            e.target.elements.price.value = '';
-            items.push(item);
-            renderItems(items);
-        })
-
+function logger(a, b) {
+    console.log(`logger: ${a} ${b}`);
 }
 
-form.addEventListener('submit', onSubmit);
+const debounced = debounce(logger, 100);
 
+debounced(1, 2);
+debounced(3, 4);
+debounced(3, 4);
+debounced(3, 4);
+debounced(3, 4);
+debounced(3, 4);
 
-const editBtns = container.querySelectorAll('#edit');
-
-console.log('buttons', editBtns);
-
-editBtns.forEach(btn => {
-    btn.addEventListener('click', () => console.log('edit', btn.dataset.id))
-})
-
-
+setTimeout(() => debounced(5, 6), 100)
