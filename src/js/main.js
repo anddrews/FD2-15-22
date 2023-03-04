74bf7f2d5e5c3c41bea7f3c3fd5b3a8c1943b3b3
andrei.kandratsenka <anddrews@tut.by>
2023-03-04 08:42:55 +0200

    local storage

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,21 @@ var observer = new IntersectionObserver((entries) => {
 })
 
 observer.observe(document.querySelector('.item'));
+
+var item = {
+    id: 1,
+    name: 'Iphone 14'
+}
+
+var item2 = {
+    id: 2,
+    name: 'Iphone 15'
+}
+
+localStorage.setItem('backet', JSON.stringify([item]));
+
+const backet = JSON.parse(localStorage.getItem('backet'));
+
+backet.push(item2);
+localStorage.setItem('backet', JSON.stringify(backet));
+
